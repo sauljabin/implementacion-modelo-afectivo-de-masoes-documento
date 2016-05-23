dd61bfd42c29082ae71ae254573c9d015668b8d8
Saúl Piña <sauljabin@gmail.com>
2016-05-22 21:41:17 -0500

    Cambios en compilacion

--- plantilla/compilar.sh
+++ plantilla/compilar.sh
@@ -1,61 +1,146 @@
-#!/bin/bash
-borrartemporales(){
-  echo "Eliminando archivos temporales"
-  rm -f $archivo.aux
-  rm -f $archivo.bbl
-  rm -f $archivo.blg
-  rm -f $archivo.glg
-  rm -f $archivo.glo
-  rm -f $archivo.gls
-  rm -f $archivo.glsdefs
-  rm -f $archivo.ist
-  rm -f $archivo.lal
-  rm -f $archivo.lol
-  rm -f $archivo.loc
-  rm -f $archivo.loi
-  rm -f $archivo.lod
-  rm -f $archivo.loq
-  rm -f $archivo.out
-  rm -f $archivo.toc
-  rm -f $archivo.log
-  rm -f $archivo.synctex.gz
-  rm -f $archivo.dvi
-  rm -f $archivo.brf
-  rm -f $archivo.lon
-  rm -f capitulos/*.aux
-}
+#!/usr/bin/env bash
+
+VERSION="v4"
+NAME="Plantilla LaTeX Trabajo de Grado"
+UNI="Universidad Centoccidental Lisandro Alvarado"
+
+TEMP_EXT=(
+  aux
+  bbl
+  blg
+  glg
+  glo
+  gls
+  glsdefs
+  ist
+  lal
+  lol
+  loc
+  loi
+  lod
+  loq
+  out
+  toc
+  log
+  synctex.gz
+  dvi
+  brf
+  lon
+  bit
+  lof
+  lot
+  glx
+  gxg
+  gxs
+  idx
+  ilg
+  ind
+  url
+  svn
+)
+
+declare -A MSG=(
+  [PRESS_KEY]="Presione una tecla para continuar..."
+  [DEL_FILE]="Eliminando archivos con extensión"
+  [DEL_ALL_FILE]="Eliminando archivos temporales"
+  [END_PROCESS]="Archivo generado"
+  [VERSION]="Versión"
+  [HELP_CLEAN]="Limpiar archivos temporales"
+  [HELP_BUILD]="Generar archivo pdf"
+  [HELP_HELP]="Ayuda"
+  [FILE_NOT_FOUND]="El archivo no existe"
+)
+
+FILE_NAME_PROJ=project
+FILE_NAME_FIN=final
+
 pause(){
-  read -p "Presione una tecla para continuar..."
+  read -p "${MSG[PRESS_KEY]}"
 }
-if  [ "$1" != "" ]; then
-  archivo=$1
-else
-  echo "Debe ingresar el nombre del archivo a compilar, ejemplo: ./compilar.sh trabajo"
-  pause
+
+clean_files() {
+  echo "${MSG[DEL_ALL_FILE]}"
+  for ext in ${TEMP_EXT[@]}
+  do
+    find . -type f -name "*.$ext" -delete
+  done
+}
+
+validate_file() {
+    if [ ! -f $1.tex ]; then
+      echo "${MSG[FILE_NOT_FOUND]} $1.tex"
+      exit
+    fi
+}
+
+generate_document() {
+  pdflatex -synctex=1 -interaction=nonstopmode $1
+  makeglossaries $1
+  makeindex $1.glo -s $1.ist -t $1.glg -o $1.gls
+  pdflatex -synctex=1 -interaction=nonstopmode $1
+  makeglossaries $1
+  makeindex $1.glo -s $1.ist -t $1.glg -o $1.gls
+  pdflatex -synctex=1 -interaction=nonstopmode $1
+  bibtex $1
+  pdflatex -synctex=1 -interaction=nonstopmode $1
+  pdflatex -synctex=1 -interaction=nonstopmode $1
+  pdflatex -synctex=1 -interaction=nonstopmode $1
+  makeglossaries $1
+  makeindex $1.glo -s $1.ist -t $1.glg -o $1.gls
+  pdflatex -synctex=1 -interaction=nonstopmode $1
+  echo "${MSG[END_PROCESS]} $1.pdf"
+}
+
+usage() {
+  echo "$UNI"
+  echo "$NAME"
+  echo "${MSG[VERSION]} $VERSION"
+  echo "   -c, clean ${MSG[HELP_CLEAN]}"
+  echo "   -b, build ${MSG[HELP_BUILD]}"
+  echo "   -h, help ${MSG[HELP_HELP]}"
+}
+
+all(){
+  validate_file $1
+  clean_files
+  generate_document $1
+  clean_files
+}
+
+run() {
+  if [ "$#" == 0 ];
+    then
+      all $FILE_NAME_FIN
+    else
+        case "$1" in
+            -c | [Cc]lean | CLEAN )
+                clean_files
+                ;;
+            -b | [Bb]uild | BUILD )
+                if  [ "$2" == "" ]
+                  then
+                    file=$FILE_NAME_FIN
+                  else
+                    file=$2
+                fi
+                validate_file $file
+                generate_document $file
+                ;;
+            -h | [Hh]elp | HELP )
+                usage
+                ;;
+            *)
+                if  [ "$1" == "" ]
+                  then
+                    file=$FILE_NAME_FIN
+                  else
+                    file=$1
+                fi
+                all $file
+                ;;
+        esac
+  fi
   exit
-fi
-if [ ! -f $archivo.tex ]; then
-  echo "Archivo $archivo.tex no existe"
-  pause
-  exit
-fi
-borrartemporales
-echo "Compilando archivo: $archivo.tex"
-pdflatex -synctex=1 -interaction=nonstopmode $archivo
-makeglossaries $archivo
-makeindex $archivo.glo -s $archivo.ist -t $archivo.glg -o $archivo.gls
-pdflatex -synctex=1 -interaction=nonstopmode $archivo
-makeglossaries $archivo
-makeindex $archivo.glo -s $archivo.ist -t $archivo.glg -o $archivo.gls
-pdflatex -synctex=1 -interaction=nonstopmode $archivo
-bibtex $archivo
-pdflatex -synctex=1 -interaction=nonstopmode $archivo
-pdflatex -synctex=1 -interaction=nonstopmode $archivo
-pdflatex -synctex=1 -interaction=nonstopmode $archivo
-makeglossaries $archivo
-makeindex $archivo.glo -s $archivo.ist -t $archivo.glg -o $archivo.gls
-pdflatex -synctex=1 -interaction=nonstopmode $archivo
-echo "Archivo generado: $archivo.pdf"
-borrartemporales
-pause
-exit
+}
+
+run $*
